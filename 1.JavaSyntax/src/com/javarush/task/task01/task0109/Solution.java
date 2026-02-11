3dc56c096b880cf6dfbd7339a32f4c13cc5ec884
pablosvd <pavel99svirid@gmail.com>
2026-02-11 20:23:12 +0300

    Добавил задачу task0101 - 0115

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+Объявление и инициализация переменных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+    }
+}
